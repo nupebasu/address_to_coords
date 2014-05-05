b9b2dd7e4f3e153b7728cce637504ab230d3b585
Anupam Basu <nupebasu@gmail.com>
2014-05-04 20:35:40 -0500

    Changes
    Still not dynamically updating

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 AddressToCoords::Application.routes.draw do
   get("/coords", { :controller => "addresses", :action => "fetch_coordinates" })
+  get("/coords/:id", { :controller => "addresses", :action => "fetch_coordinates" })
+
 end
